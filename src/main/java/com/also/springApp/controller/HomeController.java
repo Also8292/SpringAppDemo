6247d39db22bac4494a78c3d07e09441467687aa
Also8292 <sowalils1992@gmail.com>
2019-02-13 14:54:22 +0000

    spring app : database connected

--- src/main/java/com/also/springApp/controller/HomeController.java
+++ src/main/java/com/also/springApp/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.also.springApp.controller;
 
+import com.also.springApp.model.Student;
+import com.also.springApp.model.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author also
@@ -9,16 +15,36 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 
 @Controller
+@RequestMapping(path="/student")
 public class HomeController {
 
-	@GetMapping("/")
+	/*@GetMapping("/")
 	public String home() {
 		return "home";
 	}
-	
+
 	@GetMapping("/about")
 	public String about() {
 		return "about";
+	}*/
+
+	@Autowired
+
+	private StudentRepository studentRepository;
+
+	@GetMapping(path="/add")
+	public @ResponseBody String addNewStudent (@RequestParam String name, @RequestParam String email) {
+		Student a = new Student();
+		a.setName(name);
+		a.setEmail(email);
+		studentRepository.save(a);
+
+		return "Saved";
 	}
-	
+
+	@GetMapping(path="/all")
+	public @ResponseBody Iterable<Student> getAllStudents() {
+		return studentRepository.findAll();
+	}
+
 }
